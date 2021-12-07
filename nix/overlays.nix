6d474f932be622f16fbc2aaef48a4f7dbb306765
Felix Leitz <felix.leitz92@gmail.com>
2021-12-07 18:22:07 +0100

    remove unused co-log-polysemy nix derivation

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -21,13 +21,6 @@ let
             sha256 = "0y8i5c4k4q75qmq5brxdqpa39zzgywcv9ipbvz9hkd6av6f8673r";
           };
 
-          co-log-polysemySrc = super.fetchFromGitHub {
-            owner = "kowainik";
-            repo = "co-log";
-            rev = "72fbe394b437c698d574cd7604ad3f7f807383e0";
-            sha256 = "0qb9br77ywvjq1df2yfkgizwfq0pgd3ldik8q6bqsic1fs3p2pax";
-          };
-
           # Use the newest version of polysemy to circumvent dependency
           # version problems.
           polysemy =
