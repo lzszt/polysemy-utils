5b730f2fd8bfc9d5401595ac80c98167662a3a8b
Felix Leitz <felix.leitz92@gmail.com>
2021-12-07 18:01:37 +0100

    Add haddock and test runner to Time effect

--- src/Effects/Time.hs
+++ src/Effects/Time.hs
@@ -14,18 +14,26 @@ module Effects.Time
   ( Time (..),
     getTime,
     runTime,
+    runFixedTime,
   )
 where
 
 import Data.Time
 import Polysemy
 
+-- | An effect for the current `UTCTime`
 data Time r a where
   GetTime :: Time r UTCTime
 
 makeSem ''Time
 
+-- | Run `Time` effect via `Embed IO`
 runTime :: Members '[Embed IO] r => Sem (Time : r) a -> Sem r a
 runTime =
   interpret $ \case
     GetTime -> embed getCurrentTime
+
+-- | Run `Time` effect with a fixed `UTCTime`
+runFixedTime :: UTCTime -> Sem (Time : r) a -> Sem r a
+runFixedTime fixedTime =
+  interpret $ \GetTime -> pure fixedTime
