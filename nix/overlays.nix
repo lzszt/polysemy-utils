4edcb1e5ef3e1531c7fd08e4cbcd1d242daf90b7
Felix Leitz <felix.leitz92@gmail.com>
2022-03-13 21:40:56 +0100

    update nix pin

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -13,22 +13,6 @@ let
             super.haskell.lib.dontHaddock
             (super.haskell.lib.disableLibraryProfiling drv);
 
-          # A recent version of polysemy.
-          polysemySrc = super.fetchFromGitHub {
-            owner = "polysemy-research";
-            repo = "polysemy";
-            rev = "c37d485b614e98622f5e7473a478b781a6ad5c45";
-            sha256 = "0y8i5c4k4q75qmq5brxdqpa39zzgywcv9ipbvz9hkd6av6f8673r";
-          };
-
-          # Use the newest version of polysemy to circumvent dependency
-          # version problems.
-          polysemy =
-            defaultMod (hself.callCabal2nix "polysemy" polysemySrc { });
-          polysemy-plugin = defaultMod
-            (hself.callCabal2nix "polysemy" (polysemySrc + /polysemy-plugin)
-              { });
-
           co-log-polysemy =
             defaultMod (super.haskell.lib.doJailbreak hsuper.co-log-polysemy);
 
@@ -41,8 +25,6 @@ let
             hself.callCabal2nix "polysemy-utils" polysemy-utils-src { };
         in {
           inherit polysemy-utils;
-          inherit polysemy;
-          inherit polysemy-plugin;
           inherit co-log-polysemy;
         };
     };
