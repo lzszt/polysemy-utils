664257cb651f94b0e337838198b9471926a47cc3
Felix Leitz <felix.leitz92@gmail.com>
2023-03-21 19:26:05 +0100

    Fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
-(import (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-in fetchTarball {
+(import (fetchTarball {
   url =
-    "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-  sha256 = lock.nodes.flake-compat.locked.narHash;
+    "https://github.com/edolstra/flake-compat/archive/b4a34015c698c7793d592d66adbab377907a2be8.tar.gz";
+  sha256 = "sha256-Z+s0J8/r907g149rllvwhb4pKi8Wam5ij0st8PwAh+E=";
 }) { src = ./.; }).shellNix
