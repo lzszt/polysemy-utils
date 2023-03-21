4087f41223c21b119a1a534233cf9a0b57c2efba
Felix Leitz <felix.leitz92@gmail.com>
2023-03-21 18:36:30 +0100

    Generalize flake for default systems.

--- flake.nix
+++ flake.nix
@@ -3,29 +3,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, ... }:
-    let
-      pkgs = import ./nix/pkgs.nix {
-        inherit nixpkgs;
-        system = "x86_64-linux";
-      };
-      packageName = "polysemy-utils";
-    in {
-      packages.x86_64-linux.${packageName} =
-        pkgs.haskellPackages.polysemy-utils;
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.${packageName};
-      devShells.x86_64-linux = {
-        default = pkgs.haskellPackages.shellFor {
-          packages = p: [ p.polysemy-utils ];
-          nativeBuildInputs = with pkgs; [
-            haskellPackages.cabal-install
-            haskellPackages.ghc
-            haskellPackages.hlint
-            haskellPackages.ghcid
-            haskellPackages.haskell-language-server
-            haskellPackages.fourmolu
-          ];
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import ./nix/pkgs.nix { inherit nixpkgs system; };
+        packageName = "polysemy-utils";
+      in {
+        packages.${packageName} = pkgs.haskellPackages.polysemy-utils;
+        defaultPackage = self.packages.${system}.${packageName};
+        devShells = {
+          default = pkgs.haskellPackages.shellFor {
+            packages = p: [ p.polysemy-utils ];
+            nativeBuildInputs = with pkgs; [
+              haskellPackages.cabal-install
+              haskellPackages.ghc
+              haskellPackages.hlint
+              haskellPackages.ghcid
+              haskellPackages.haskell-language-server
+              haskellPackages.fourmolu
+            ];
+          };
         };
-      };
-    };
+      });
 }
