c8837c1a9f849d3f53a499cea2b17e20a83833fe
Felix Leitz <felix.leitz92@gmail.com>
2024-01-17 22:22:46 +0100

    Adds rate limiting to request effect.

--- src/Effects/Request.hs
+++ src/Effects/Request.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Effects.Request where
 
+import Data.List.NonEmpty qualified as NE
+import Data.Maybe
+import Data.Time
 import Effects.Cache
+import Effects.Delay
+import Effects.Logging
+import Effects.Time
 import Polysemy
+import Polysemy.State qualified as State
 
 data Request x y r a where
   Request :: x -> Request x y r y
@@ -22,7 +30,6 @@ runRequest interpreterFn = interpret $ \case
   Request x -> embed $ interpreterFn x
 
 cachedRequest ::
-  (Members '[] r) =>
   Sem (Request x y : r) a ->
   Sem (Cache x y : Request x y : r) a
 cachedRequest =
@@ -35,3 +42,95 @@ cachedRequest =
           y <- request x
           update x y
           pure y
+
+data RateSpec a = RateSpec
+  { interval :: NominalDiffTime,
+    numberRequests :: a
+  }
+
+saveMaximum :: (Ord a) => [a] -> Maybe a
+saveMaximum = go Nothing
+  where
+    go acc [] = acc
+    go Nothing (x : xs) = go (Just x) xs
+    go acc@(Just y) (x : xs)
+      | x > y = go (Just x) xs
+      | otherwise = go acc xs
+
+requestDelay ::
+  ( Members
+      '[ State.State [(UTCTime, x)],
+         Time,
+         Log
+       ]
+      r
+  ) =>
+  (Num l) =>
+  (Ord l) =>
+  NE.NonEmpty (RateSpec l) ->
+  (x -> l) ->
+  l ->
+  Sem r (Maybe NominalDiffTime)
+requestDelay rateSpecs requestCost nextRequestCost = do
+  now <- getTime
+  requestTimes <- State.get
+  pure
+    $ saveMaximum
+    $ mapMaybe
+      ( \RateSpec {..} ->
+          let intervalStart = addUTCTime (negate interval) now
+              requestsInInterval = filter ((>= intervalStart) . fst) requestTimes
+              earliestRequestInInterval = minimum $ map fst requestsInInterval
+              requestsInIntervalCost = nextRequestCost + sum (map (requestCost . snd) requestsInInterval)
+           in if requestsInIntervalCost < numberRequests
+                then Nothing
+                else Just $ diffUTCTime earliestRequestInInterval intervalStart
+      )
+    $ NE.toList rateSpecs
+
+rateLimitedRequest' ::
+  ( Members
+      '[ State.State [(UTCTime, x)],
+         Time,
+         Delay,
+         Log
+       ]
+      r
+  ) =>
+  (Num l) =>
+  (Ord l) =>
+  NE.NonEmpty (RateSpec l) ->
+  (x -> l) ->
+  Sem (Request x y : r) a ->
+  Sem (Request x y : r) a
+rateLimitedRequest' rateSpecs requestCost =
+  reinterpret $ \case
+    Request x -> do
+      now <- getTime
+      potentialRequestDelay <- requestDelay rateSpecs requestCost (requestCost x)
+      logDebug $ "Rate limiting delay: " <> show potentialRequestDelay
+      case potentialRequestDelay of
+        Nothing -> do
+          State.modify' ((now, x) :)
+          request x
+        Just d -> do
+          delay d
+          delayedNow <- getTime
+          State.modify' ((delayedNow, x) :)
+          request x
+
+rateLimitedRequest ::
+  ( Members
+      '[ State.State [(UTCTime, x)],
+         Time,
+         Delay,
+         Log
+       ]
+      r
+  ) =>
+  (Num l) =>
+  (Ord l) =>
+  NE.NonEmpty (RateSpec l) ->
+  Sem (Request x y : r) a ->
+  Sem (Request x y : r) a
+rateLimitedRequest rateSpecs = rateLimitedRequest' rateSpecs (const 1)
